a0c1067b28010847d33f7af2eedae19cd64fbb84
Hunter Parks <parks.hunter1992@gmail.com>
2021-10-14 12:19:37 -0500

    going over some basic variables. Java - Udemy Course

--- src/java_basics/Variables.java
+++ src/java_basics/Variables.java
@@ -2,6 +2,10 @@ package java_basics;
 
 public class Variables {
     public static void main(String[] args) {
+        int x = 34;
+        System.out.println(x);
 
+        String words = "This is a sentence.";
+        System.out.println(words);
     }
 }
