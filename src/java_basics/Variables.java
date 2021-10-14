24387dec28c26405b5b7161e6a86f7c200f0daff
Hunter Parks <parks.hunter1992@gmail.com>
2021-10-14 12:25:20 -0500

    going over some datatypes

--- src/java_basics/Variables.java
+++ src/java_basics/Variables.java
@@ -7,5 +7,13 @@ public class Variables {
 
         String words = "This is a sentence.";
         System.out.println(words);
+
+        // Datatypes
+        int var = -100000000;
+        short bigNumber = -32768;
+        byte reallySmallNumber = -128;
+        double decimalVariable = 394.003;
+        boolean decision = false;
+        char letter = '9';
     }
 }
