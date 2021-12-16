c16cc23bf5d36334b241708e355024c74375d6ef
Hunter Parks <parks.hunter1992@gmail.com>
2021-12-16 16:22:55 -0600

    finishing up learning arrays lec

--- src/java_basics/Learning_Arrays.java
+++ src/java_basics/Learning_Arrays.java
@@ -3,6 +3,18 @@ package java_basics;
 public class Learning_Arrays {
     public static void main(String[] args) {
 
+        //Example #1
         int [] values = new int[100];
+        values[0] = 1000;
+        values[99] = 93432;
+
+        System.out.println(values[99]);
+
+        // Example #2
+        String [] words = new String[] {"My", "name", "is"};
+
+        System.out.println(words[2]);
+
+
     }
 }
