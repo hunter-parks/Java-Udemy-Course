757ce363d30c6d99af324a66d4db121326db3229
Hunter Parks <parks.hunter1992@gmail.com>
2022-04-04 15:24:29 -0500

    finished up Learning Methods - Java Udemy Course

--- src/java_basics/LearningMethods.java
+++ src/java_basics/LearningMethods.java
@@ -4,7 +4,11 @@ public class LearningMethods {
     public static void main(String[] args) {
         System.out.println("Hello!");
         MyUtils.printSomeJunk("this is some data");
-        MyUtils.printSomeJunk(34);
 
+        System.out.println(MyUtils.printSomeJunk("hello there"));
+        MyUtils.sum2Numbers(3,2);
+
+       int myvar = MyUtils.add10(10);
+        System.out.println(myvar);
     }
 }
--- src/java_basics/MyUtils.java
+++ src/java_basics/MyUtils.java
@@ -2,14 +2,22 @@ package java_basics;
 
 public class MyUtils {
 
-    public static void printSomeJunk(String argument) {
-        System.out.println("Some bla bla bla " + argument);
-
-
+    public static String printSomeJunk(String argument) {
+//        System.out.println("Some bla bla bla " + argument);
+        return argument;
     }
 
     public static void printSomeJunk(int argument) {
         System.out.println("Integer passed in: " + argument);
 
+    }
+
+    public static void sum2Numbers (int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static int add10(int someArgument) {
+        int result = someArgument + 10;
+        return result;
     }
 }
