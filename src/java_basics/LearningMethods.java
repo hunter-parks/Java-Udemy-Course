afe61fb56cdbb08c15402e9811d8c48d7dc592fa
Hunter Parks <parks.hunter1992@gmail.com>
2022-04-04 15:01:16 -0500

    defining methods in Java. Halfway through Learning Methods - Java Udemy Course

--- src/java_basics/LearningMethods.java
+++ src/java_basics/LearningMethods.java
@@ -2,7 +2,9 @@ package java_basics;
 
 public class LearningMethods {
     public static void main(String[] args) {
+        System.out.println("Hello!");
+        MyUtils.printSomeJunk("this is some data");
+        MyUtils.printSomeJunk(34);
 
-        System.out.println();
     }
 }
---
+++ src/java_basics/MyUtils.java
@@ -0,0 +1,15 @@
+package java_basics;
+
+public class MyUtils {
+
+    public static void printSomeJunk(String argument) {
+        System.out.println("Some bla bla bla " + argument);
+
+
+    }
+
+    public static void printSomeJunk(int argument) {
+        System.out.println("Integer passed in: " + argument);
+
+    }
+}
